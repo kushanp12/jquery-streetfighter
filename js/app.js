91c383ee41358f084ac8e3bdc959dc908f8608c5
kushp12 <kushanp12@gmail.com>
2014-09-29 15:35:48 -0400

    Bottom_margin

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// When the users' cursor enters .ryu, an animation of ryu will be displayed. //
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -7,17 +8,18 @@ $(document).ready(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
+	// When the mouse is clicked, ryu is shown throwing his Hadouken attack //
 	.mousedown(function(){
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
 		.animate(
-			{'left': '900px'},
+			{'left': '1200px'},
 			500,
 			function(){
 				$(this).hide();
-				$(this).css('left', '569px');
+				$(this).css('left', '969px'; 'opacity', '0.1');
 			}
 		);
 	})
@@ -25,6 +27,7 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	// When X is held down, Ryu strikes a cool pose//
 	$('body').keydown(function(event){
 		if (event.which == 88){
 			$('.ryu-still').hide();
@@ -39,7 +42,7 @@ $(document).ready(function(){
 		}
 	});
 });
-
+// Plays the audio clips of Ryu//
 function playHadouken(){
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
